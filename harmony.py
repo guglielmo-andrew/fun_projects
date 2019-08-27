680d747f76bc6a41cba9fedef952a62ef5e9de78
Andrew Guglielmo <23464959+guglielmo-andrew@users.noreply.github.com>
2019-08-27 06:47:36 -0700

    Delete harmony.py

--- harmony.py
+++
@@ -1,8 +0,0 @@
-def harmony(numbers):
-    y = len(numbers)
-    for i in range(len(numbers)):
-        smaller = numbers[i:(y - i)]
-        if max(smaller) - min(smaller) == 1:
-            return len(smaller)
-
-harmony([1,3,2,2,5,2,3,7])
